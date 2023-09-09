408d3d93f733fe5b68b9f6d498f62c0535d405d0
AVEZ QURESHI <avezqureshi4785@gmail.com>
2023-09-09 14:23:14 +0530

    Crud DONE and error handling done

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,45 +1,117 @@
 const Product = require("../models/productModel");
-
+const ErrorHandler = require("../utils/errorhandler");
 
 //Create Product -- Admin
 exports.createProduct = async (req, res, next) => {
-    // let images = [];
-  
-    // if (typeof req.body.images === "string") {
-    //   images.push(req.body.images);
-    // } else {
-    //   images = req.body.images;
-    // }
-  
-    // const imagesLinks = [];
-  
-    // for (let i = 0; i < images.length; i++) {
-    //   const result = await cloudinary.v2.uploader.upload(images[i], {
-    //     folder: "products",
-    //   });
-  
-    //   imagesLinks.push({
-    //     public_id: result.public_id,
-    //     url: result.secure_url,
-    //   });
-    // }
-  
-    // req.body.images = imagesLinks;
-    // req.body.user = req.user.id;
-  
-    const product = await Product.create(req.body);
-  
-    res.status(201).json({
-      success: true,
-      product,
-    });
-  };
+  // let images = [];
+
+  // if (typeof req.body.images === "string") {
+  //   images.push(req.body.images);
+  // } else {
+  //   images = req.body.images;
+  // }
+
+  // const imagesLinks = [];
+
+  // for (let i = 0; i < images.length; i++) {
+  //   const result = await cloudinary.v2.uploader.upload(images[i], {
+  //     folder: "products",
+  //   });
+
+  //   imagesLinks.push({
+  //     public_id: result.public_id,
+  //     url: result.secure_url,
+  //   });
+  // }
+
+  // req.body.images = imagesLinks;
+  // req.body.user = req.user.id;
+
+  const product = await Product.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    product,
+  });
+};
 
 //get all Products
-exports.getAllProducts = async(req,res) => {
-    const products = await Product.find();
+exports.getAllProducts = async (req, res) => {
+  const products = await Product.find();
+  res.status(200).json({
+    success: true,
+    products,
+  });
+};
+
+//update product
+exports.updateProduct = async (req, res, next) => {
+  try {
+    // Find the product by ID
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return next(new ErrorHandler("Product not found", 404));
+    }
+
+    // Update the product with the data from req.body
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true, // Return the updated product
+        runValidators: true, // Run validation on the update
+        useFindAndModify: false, // Use the new update method
+      }
+    );
+
     res.status(200).json({
-        success:true,
-        products
-    })
-}
+      success: true,
+      product: updatedProduct,
+    });
+  } catch (error) {
+    // Handle any errors that may occur during the update
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
+
+exports.deleteProduct = async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return next(new ErrorHandler("Product not found", 404));
+    }
+
+    // Use deleteOne to remove the product
+    await Product.deleteOne({ _id: req.params.id });
+
+    res.status(200).json({
+      success: true,
+      message: "Product deleted successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
+
+// Get Product Details
+exports.getProductDetails = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
